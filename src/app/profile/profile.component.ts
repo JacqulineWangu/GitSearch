bb9918f831aa8af6616c075a6a9aa17159bcaec4
jacquline <jacqulinewangu@gmail.com>
2019-05-06 15:19:41 +0300

    added content to the files including putting the Api key in environment.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -14,10 +14,18 @@ repos:any[];
     this.profileService.getProfileInfo().subscribe(profile => {
       console.log(profile);
       this.profile = profile;
-    })
-   }
+    });
+
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    
+   })
+  }
 
   ngOnInit() {
   }
 
-}
+  }
+
+
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/common/http';
+import { HttpClient, } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProfileService {
 
-  private username:string;
-  private clientid  "";
-  private clientsecret  "";
+  private username: string;
+  private clientid = "";
+  private clientsecret = "";
 
-  constructor(private http:Http) { 
+  constructor(private http: HttpClient) {
     console.log("service is now ready!");
-    this.username 'JacqulineWangu';
+    this.username = 'JacqulineWangu';
   }
 
-  getProfileInfo(){
-    return this.http.get("https://api.github.com/users/"  this.username "?client_id."  this.clientid );
+  getProfileInfo() {
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id." + this.clientid + "&client_secret=" + this.clientsecret)
+    .map(res => res.json());
+  }
+
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id." + this.clientid + "&client_secret=" + this.clientsecret)
+    .map(res => res.json());
   }
 }
