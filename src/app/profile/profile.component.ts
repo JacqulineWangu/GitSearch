875d0b403e6783dd1ca3c75886a761a713508aa7
jacquline <jacqulinewangu@gmail.com>
2019-05-06 07:44:22 +0300

    added a file on profile service

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProfileService } from '../../'
 @Component({
-  selector: 'app-profile',
+  selector: 'gg-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+profile:any[];
+repos:any[];
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.repos = repos;
+    })
+   }
 
   ngOnInit() {
   }
